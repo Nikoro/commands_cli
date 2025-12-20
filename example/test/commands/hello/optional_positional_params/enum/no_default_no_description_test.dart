cf4f0fb761bd08bd259664b3fb2938d2192f5ff9
Dominik Krajcer <dom.kraj@gmail.com>
2025-12-20 23:47:12 +0100

    test(hello/optional_positional_params): add integration tests for enum param with/without default and description

---
+++ example/test/commands/hello/optional_positional_params/enum/no_default_no_description_test.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../integration_tests.dart';
+
+void main() {
+  integrationTests(
+    '''
+        hello:
+          script: echo "Hello {name}"
+          params:
+            optional:
+              - name:
+                values: [Alpha, Bravo, Charlie]
+    ''',
+    () {
+      for (Object param in ['Alpha', 'Bravo', 'Charlie']) {
+        test('prints "Hello $param"', () async {
+          final result = await Process.run('hello', ['$param']);
+          expect(result.stdout, equals('Hello $param\n'));
+        });
+      }
+
+      test('prints "Hello " when no optional param is specified', () async {
+        final result = await Process.run('hello', []);
+        expect(result.stdout, equals('Hello \n'));
+      });
+
+      test('prints error when invalid value for optional param is specified', () async {
+        final result = await Process.run('hello', ['Delta']);
+        expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid value: "Delta"
+💡 Must be one of: $bold${green}Alpha$reset, $bold${green}Bravo$reset, $bold${green}Charlie$reset
+'''));
+      });
+
+      for (String flag in ['-h', '--help']) {
+        test('$flag prints help', () async {
+          final result = await Process.run('hello', [flag]);
+          expect(result.stdout, equals('''
+${blue}hello$reset
+params:
+  optional:
+    ${magenta}name$reset
+    ${bold}values$reset: Alpha, Bravo, Charlie
+'''));
+        });
+      }
+    },
+  );
+}
---
+++ example/test/commands/hello/optional_positional_params/enum/with_default_no_description_test.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../integration_tests.dart';
+
+void main() {
+  integrationTests(
+    '''
+        hello:
+          script: echo "Hello {name}"
+          params:
+            optional:
+              - name:
+                values: [Alpha, Bravo, Charlie]
+                default: Charlie
+    ''',
+    () {
+      for (Object param in ['Alpha', 'Bravo', 'Charlie']) {
+        test('prints "Hello $param"', () async {
+          final result = await Process.run('hello', ['$param']);
+          expect(result.stdout, equals('Hello $param\n'));
+        });
+      }
+
+      test('prints with default value if none specified', () async {
+        final result = await Process.run('hello', []);
+        expect(result.stdout, equals('Hello Charlie\n'));
+      });
+
+      test('prints error when invalid value for optional param is specified', () async {
+        final result = await Process.run('hello', ['Delta']);
+        expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid value: "Delta"
+💡 Must be one of: $bold${green}Alpha$reset, $bold${green}Bravo$reset, $bold${green}Charlie$reset
+'''));
+      });
+
+      for (String flag in ['-h', '--help']) {
+        test('$flag prints help', () async {
+          final result = await Process.run('hello', [flag]);
+          expect(result.stdout, equals('''
+${blue}hello$reset
+params:
+  optional:
+    ${magenta}name$reset
+    ${bold}values$reset: Alpha, Bravo, Charlie
+    ${bold}default$reset: "Charlie"
+'''));
+        });
+      }
+    },
+  );
+
+  integrationTests(
+    '''
+        hello:
+          script: echo "Hello {name}"
+          params:
+            optional:
+              - name:
+                values: [Alpha, Bravo, Charlie]
+                default: Delta
+    ''',
+    () {
+      for (Object param in ['Alpha', 'Bravo', 'Charlie']) {
+        test('prints error when invalid default value is specified', () async {
+          final result = await Process.run('hello', ['$param']);
+          expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid default: "Delta"
+💡 Must be one of: $bold${green}Alpha$reset, $bold${green}Bravo$reset, $bold${green}Charlie$reset
+'''));
+        });
+      }
+
+      test('prints error when invalid default value is specified', () async {
+        final result = await Process.run('hello', []);
+        expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid default: "Delta"
+💡 Must be one of: $bold${green}Alpha$reset, $bold${green}Bravo$reset, $bold${green}Charlie$reset
+'''));
+      });
+
+      test('prints error when invalid default value is specified', () async {
+        final result = await Process.run('hello', ['Delta']);
+        expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid default: "Delta"
+💡 Must be one of: $bold${green}Alpha$reset, $bold${green}Bravo$reset, $bold${green}Charlie$reset
+'''));
+      });
+
+      for (String flag in ['-h', '--help']) {
+        test('prints error when invalid default value is specified', () async {
+          final result = await Process.run('hello', [flag]);
+          expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid default: "Delta"
+💡 Must be one of: $bold${green}Alpha$reset, $bold${green}Bravo$reset, $bold${green}Charlie$reset
+'''));
+        });
+      }
+    },
+  );
+}
