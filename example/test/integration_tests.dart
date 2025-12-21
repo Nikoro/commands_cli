031b2df2bbe111413e96160e0559d6e51c469ca3
Dominik Krajcer <dom.kraj@gmail.com>
2025-12-21 23:40:05 +0100

    fix(test): serialize file access in integration tests
    Add a simple file lock to ensure commands.yaml is not accessed concurrently by test groups. This prevents race conditions and test flakiness when running integration tests in parallel.

--- example/test/integration_tests.dart
+++ example/test/integration_tests.dart
@@ -1,20 +1,42 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:meta/meta.dart' show isTestGroup;
 import 'package:test/test.dart';
 
 final _commandsYaml = File('commands.yaml');
+final _lock = _FileLock();
+
+/// Simple lock to serialize file access across test groups
+class _FileLock {
+  var _current = Future<void>.value();
+
+  Future<T> synchronized<T>(Future<T> Function() action) async {
+    final previous = _current;
+    final completer = Completer<void>();
+    _current = completer.future;
+
+    await previous;
+    try {
+      return await action();
+    } finally {
+      completer.complete();
+    }
+  }
+}
 
 @isTestGroup
 void integrationTests(String description, dynamic Function() body) => group(description, () {
       late String originalContent;
 
-      setUpAll(() {
-        originalContent = _commandsYaml.readAsStringSync();
-        _commandsYaml.writeAsStringSync(description);
-      });
+      setUpAll(() async => await _lock.synchronized(() async {
+            originalContent = await _commandsYaml.readAsString();
+            await _commandsYaml.writeAsString(description);
+          }));
 
-      tearDownAll(() => _commandsYaml.writeAsStringSync(originalContent));
+      tearDownAll(() async => await _lock.synchronized(() async {
+            await _commandsYaml.writeAsString(originalContent);
+          }));
 
       body();
     });
