ab36060a7e5386c6213d66d0330c073894d03c32
Dominik Krajcer <dom.kraj@gmail.com>
2026-01-13 16:21:27 +0100

    test(hello/switch/nested/params): add single param test cases for optional/required named/positional with all default/description variants

---
+++ example/test/commands/hello/switch/nested/params/required_named/single/with_custom_default_with_full_description_test.dart
@@ -0,0 +1,101 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../integration_tests.dart';
+
+void main() {
+  integrationTests(
+    '''
+        hello: ## Description of command hello
+          switch:
+            - level1a: ## Description of level 1a
+              switch:
+                - level2a: ## Description of level 1a 2a
+                  script: echo "Hello {name}"
+                  params:
+                    required:
+                      - name: '-n, --name, nm' ## Description of parameter name
+                - level2b: ## Description of level 1a 2b
+                  script: echo "Level 1a 2b"
+                - level2c: ## Description of level 1a 2c
+                  script: echo "Level 1a 2c"
+                - default: ## Description of level 1a Custom
+                  script: echo "Level 1a Custom"                 
+            - level1b: ## Description of level 1b
+              script: echo "Level 1b"
+            - level1c: ## Description of level 1c
+              script: echo "Level 1c"
+            - default: ## Description of Custom
+              script: echo "Custom"
+    ''',
+    () {
+      test('runs default option when option with nested switch is not specified', () async {
+        final result = await Process.run('hello', ['level1a']);
+        expect(result.stdout, equals('Level 1a Custom\n'));
+      });
+
+      for (String flag in ['-n', '--name', 'nm']) {
+        for (Object param in ['World', 1, 2.2, true]) {
+          test('prints "Hello $param"', () async {
+            final result = await Process.run('hello', ['level1a', 'level2a', flag, '$param']);
+            expect(result.stdout, equals('Hello $param\n'));
+          });
+        }
+      }
+
+      test('prints error when no required param is specified', () async {
+        final result = await Process.run('hello', ['level1a', 'level2a']);
+        expect(result.stderr, equals('❌ Missing required named param: $bold${red}name$reset\n'));
+      });
+
+      test('prints "Level 1a 2b"', () async {
+        final result = await Process.run('hello', ['level1a', 'level2b']);
+        expect(result.stdout, equals('Level 1a 2b\n'));
+      });
+
+      test('prints "Level 1a 2c"', () async {
+        final result = await Process.run('hello', ['level1a', 'level2c']);
+        expect(result.stdout, equals('Level 1a 2c\n'));
+      });
+
+      test('prints "Level 1b"', () async {
+        final result = await Process.run('hello', ['level1b']);
+        expect(result.stdout, equals('Level 1b\n'));
+      });
+
+      test('prints "Level 1c"', () async {
+        final result = await Process.run('hello', ['level1c']);
+        expect(result.stdout, equals('Level 1c\n'));
+      });
+
+      test('runs default option when no option is specified', () async {
+        final result = await Process.run('hello', []);
+        expect(result.stdout, equals('Custom\n'));
+      });
+
+      for (String flag in ['-h', '--help']) {
+        test('$flag prints help', () async {
+          final result = await Process.run('hello', [flag]);
+          expect(result.stdout, equals('''
+${blue}hello$reset: ${gray}Description of command hello$reset
+options:
+  ${blue}level1a$reset: ${gray}Description of level 1a$reset
+  options:
+    ${blue}level2a$reset: ${gray}Description of level 1a 2a$reset
+    params:
+      required:
+        ${magenta}name (-n, --name, nm)$reset ${gray}Description of parameter name$reset
+    ${blue}level2b$reset: ${gray}Description of level 1a 2b$reset
+    ${blue}level2c$reset: ${gray}Description of level 1a 2c$reset
+    ${bold}default$reset: ${gray}Description of level 1a Custom$reset
+  ${blue}level1b$reset: ${gray}Description of level 1b$reset
+  ${blue}level1c$reset: ${gray}Description of level 1c$reset
+  ${bold}default$reset: ${gray}Description of Custom$reset
+'''));
+        });
+      }
+    },
+  );
+}
