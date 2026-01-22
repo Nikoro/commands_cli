63b1e996320e0cab7dfaa46898c33853344a5805
Dominik Krajcer <dom.kraj@gmail.com>
2026-01-22 23:28:45 +0100

    test(commands/hello): add tests for required positional number param\n\nAdd tests for required positional parameter of type 'number' with and without default and description. Covers both valid and invalid cases for number/num types.

---
+++ example/test/commands/hello/params/required_positional/single/type/number/with_default_with_description_test.dart
@@ -0,0 +1,166 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../integration_tests.dart';
+
+void main() {
+  for (String type in ['number', 'num']) {
+    for (num def in [1, 1.0]) {
+      integrationTests(
+        '''
+        hello: ## Description of command hello
+          script: echo "Hello {name}"
+          params:
+            required:
+              - name: ## Description of parameter name
+                type: $type
+                default: $def
+    ''',
+        () {
+          for (num value in [1, 2.0, -3, -4.7]) {
+            test('prints "Hello $value', () async {
+              final result = await Process.run('hello', ['$value']);
+              expect(result.stdout, equals('Hello $value\n'));
+            });
+          }
+
+          test('prints "Hello $def" when no required param is specified', () async {
+            final result = await Process.run('hello', []);
+            expect(result.stdout, equals('Hello $def\n'));
+          });
+
+          test('prints error when value is boolean', () async {
+            final result = await Process.run('hello', ['true']);
+            expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset expects a ${gray}[number]$reset
+   Got: true ${gray}[boolean]$reset
+💡 Example: $bgGreen${black}hello 3$reset
+'''));
+          });
+
+          for (String value in [
+            'text',
+            "text",
+            '"1"',
+            '\"1\"',
+            '\'1.5\'',
+            "'1.5'",
+            '\"true\"',
+            '"true"',
+            '\'false\'',
+            "'false'"
+          ]) {
+            test('prints error when value is string ($value)', () async {
+              final result = await Process.run('hello', [value]);
+              expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset expects a ${gray}[number]$reset
+   Got: $value ${gray}[string]$reset
+💡 Example: $bgGreen${black}hello 3$reset
+'''));
+            });
+          }
+
+          for (String flag in ['-h', '--help']) {
+            test('$flag prints help', () async {
+              final result = await Process.run('hello', [flag]);
+              expect(result.stdout, equals('''
+${blue}hello$reset: ${gray}Description of command hello$reset
+params:
+  required:
+    ${magenta}name$reset ${gray}[number] Description of parameter name$reset
+    ${bold}default$reset: $bold${orange}$def$reset
+'''));
+            });
+          }
+        },
+      );
+
+      for (final invalid in [
+        (input: true, type: 'boolean'),
+        (input: "'false'", type: 'string'),
+        (input: '"true"', type: 'string'),
+        (input: 'text', type: 'string'),
+      ]) {
+        integrationTests(
+          '''
+        hello: ## Description of command hello
+          script: echo "Hello {name}"
+          params:
+            required:
+              - name: ## Description of parameter name
+                type: $type
+                default: ${invalid.input}
+    ''',
+          () {
+            for (num value in [1, 2.0, -3, -4.7]) {
+              test('prints error', () async {
+                final result = await Process.run('hello', ['$value']);
+                expect(
+                    result.stderr,
+                    equals(
+                        '❌ Parameter $bold${red}name$reset is declared as type ${gray}[number]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+              });
+            }
+
+            test('prints error', () async {
+              final result = await Process.run('hello', []);
+              expect(
+                  result.stderr,
+                  equals(
+                      '❌ Parameter $bold${red}name$reset is declared as type ${gray}[number]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+            });
+
+            test('prints error', () async {
+              final result = await Process.run('hello', ['2']);
+              expect(
+                  result.stderr,
+                  equals(
+                      '❌ Parameter $bold${red}name$reset is declared as type ${gray}[number]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+            });
+
+            test('prints error', () async {
+              final result = await Process.run('hello', ['1.5']);
+              expect(
+                  result.stderr,
+                  equals(
+                      '❌ Parameter $bold${red}name$reset is declared as type ${gray}[number]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+            });
+
+            for (String value in [
+              'text',
+              "text",
+              '"1"',
+              '\"1\"',
+              '\'1.5\'',
+              "'1.5'",
+              '\"true\"',
+              '"true"',
+              '\'false\'',
+              "'false'"
+            ]) {
+              test('prints error', () async {
+                final result = await Process.run('hello', [value]);
+                expect(
+                    result.stderr,
+                    equals(
+                        '❌ Parameter $bold${red}name$reset is declared as type ${gray}[number]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+              });
+            }
+
+            for (String flag in ['-h', '--help']) {
+              test('prints error', () async {
+                final result = await Process.run('hello', [flag]);
+                expect(
+                    result.stderr,
+                    equals(
+                        '❌ Parameter $bold${red}name$reset is declared as type ${gray}[number]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+              });
+            }
+          },
+        );
+      }
+    }
+  }
+}
