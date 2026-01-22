9718bc7ed49432f976b01a4a0bfa033d4ccecd9c
Dominik Krajcer <dom.kraj@gmail.com>
2026-01-22 23:09:54 +0100

    test(commands/hello): add tests for optional positional number param\n\nAdd tests for optional positional parameter of type 'number' with and without default and description. Covers both valid and invalid cases for number/num types.

---
+++ example/test/commands/hello/params/optional_positional/single/type/number/no_default_no_description_test.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../integration_tests.dart';
+
+void main() {
+  for (String type in ['number', 'num']) {
+    integrationTests(
+      '''
+        hello:
+          script: echo "Hello {name}"
+          params:
+            optional:
+              - name:
+                type: $type
+    ''',
+      () {
+        for (num value in [1, 2.0, -3, -4.7]) {
+          test('prints "Hello $value', () async {
+            final result = await Process.run('hello', ['$value']);
+            expect(result.stdout, equals('Hello $value\n'));
+          });
+        }
+
+        test('prints "Hello " when no optional param is specified', () async {
+          final result = await Process.run('hello', []);
+          expect(result.stdout, equals('Hello \n'));
+        });
+
+        test('prints error when value is boolean', () async {
+          final result = await Process.run('hello', ['true']);
+          expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset expects a ${gray}[number]$reset
+   Got: true ${gray}[boolean]$reset
+💡 Example: $bgGreen${black}hello 3$reset
+'''));
+        });
+
+        for (String value in [
+          'text',
+          "text",
+          '"1"',
+          '\"1\"',
+          '\'1.5\'',
+          "'1.5'",
+          '\"true\"',
+          '"true"',
+          '\'false\'',
+          "'false'"
+        ]) {
+          test('prints error when value is string ($value)', () async {
+            final result = await Process.run('hello', [value]);
+            expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset expects a ${gray}[number]$reset
+   Got: $value ${gray}[string]$reset
+💡 Example: $bgGreen${black}hello 3$reset
+'''));
+          });
+        }
+
+        for (String flag in ['-h', '--help']) {
+          test('$flag prints help', () async {
+            final result = await Process.run('hello', [flag]);
+            expect(result.stdout, equals('''
+${blue}hello$reset
+params:
+  optional:
+    ${magenta}name$reset ${gray}[number]$reset
+'''));
+          });
+        }
+      },
+    );
+  }
+}
