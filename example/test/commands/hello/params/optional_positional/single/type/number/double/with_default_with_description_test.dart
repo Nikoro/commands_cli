e98e538f825f7ea8c11f71632b2fb7c6196d5793
Dominik Krajcer <dom.kraj@gmail.com>
2026-01-22 20:03:56 +0100

    test(hello/params): add optional positional double param tests
    add tests for optional positional double param with/without default and description
    restore example/commands.yaml to full test set

---
+++ example/test/commands/hello/params/optional_positional/single/type/number/double/with_default_with_description_test.dart
@@ -0,0 +1,172 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../../integration_tests.dart';
+
+void main() {
+  integrationTests(
+    '''
+        hello: ## Description of command hello
+          script: echo "Hello {name}"
+          params:
+            optional:
+              - name: ## Description of parameter name
+                type: double
+                default: 1.0
+    ''',
+    () {
+      for (double value in [1.0, 1.5, -2.7]) {
+        test('prints "Hello $value', () async {
+          final result = await Process.run('hello', ['$value']);
+          expect(result.stdout, equals('Hello $value\n'));
+        });
+      }
+
+      test('prints "Hello 1.0" when no optional param is specified', () async {
+        final result = await Process.run('hello', []);
+        expect(result.stdout, equals('Hello 1.0\n'));
+      });
+
+      test('prints error when value is boolean', () async {
+        final result = await Process.run('hello', ['true']);
+        expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset expects a ${gray}[double]$reset
+   Got: true ${gray}[boolean]$reset
+💡 Example: $bgGreen${black}hello 3.14$reset
+'''));
+      });
+
+      test('prints error when value is integer', () async {
+        final result = await Process.run('hello', ['1']);
+        expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset expects a ${gray}[double]$reset
+   Got: 1 ${gray}[integer]$reset
+💡 Example: $bgGreen${black}hello 3.14$reset
+'''));
+      });
+
+      for (String value in [
+        'text',
+        "text",
+        '"1"',
+        '\"1\"',
+        '\'1.5\'',
+        "'1.5'",
+        '\"true\"',
+        '"true"',
+        '\'false\'',
+        "'false'"
+      ]) {
+        test('prints error when value is string ($value)', () async {
+          final result = await Process.run('hello', [value]);
+          expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset expects a ${gray}[double]$reset
+   Got: $value ${gray}[string]$reset
+💡 Example: $bgGreen${black}hello 3.14$reset
+'''));
+        });
+      }
+
+      for (String flag in ['-h', '--help']) {
+        test('$flag prints help', () async {
+          final result = await Process.run('hello', [flag]);
+          expect(result.stdout, equals('''
+${blue}hello$reset: ${gray}Description of command hello$reset
+params:
+  optional:
+    ${magenta}name$reset ${gray}[double] Description of parameter name$reset
+    ${bold}default$reset: $bold${orange}1.0$reset
+'''));
+        });
+      }
+    },
+  );
+
+  for (final invalid in [
+    (input: 1, type: 'integer'),
+    (input: true, type: 'boolean'),
+    (input: "'false'", type: 'string'),
+    (input: '"true"', type: 'string'),
+    (input: 'text', type: 'string'),
+  ]) {
+    integrationTests(
+      '''
+        hello: ## Description of command hello
+          script: echo "Hello {name}"
+          params:
+            optional:
+              - name: ## Description of parameter name '-n, --name'
+                type: double
+                default: ${invalid.input}
+    ''',
+      () {
+        for (double value in [1.0, 1.5, -2.7]) {
+          test('prints error', () async {
+            final result = await Process.run('hello', ['$value']);
+            expect(
+                result.stderr,
+                equals(
+                    '❌ Parameter $bold${red}name$reset is declared as type ${gray}[double]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+          });
+        }
+
+        test('prints error', () async {
+          final result = await Process.run('hello', []);
+          expect(
+              result.stderr,
+              equals(
+                  '❌ Parameter $bold${red}name$reset is declared as type ${gray}[double]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+        });
+
+        test('prints error', () async {
+          final result = await Process.run('hello', ['2']);
+          expect(
+              result.stderr,
+              equals(
+                  '❌ Parameter $bold${red}name$reset is declared as type ${gray}[double]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+        });
+
+        test('prints error', () async {
+          final result = await Process.run('hello', ['1.5']);
+          expect(
+              result.stderr,
+              equals(
+                  '❌ Parameter $bold${red}name$reset is declared as type ${gray}[double]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+        });
+
+        for (String value in [
+          'text',
+          "text",
+          '"1"',
+          '\"1\"',
+          '\'1.5\'',
+          "'1.5'",
+          '\"true\"',
+          '"true"',
+          '\'false\'',
+          "'false'"
+        ]) {
+          test('prints error', () async {
+            final result = await Process.run('hello', [value]);
+            expect(
+                result.stderr,
+                equals(
+                    '❌ Parameter $bold${red}name$reset is declared as type ${gray}[double]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+          });
+        }
+
+        for (String flag in ['-h', '--help']) {
+          test('prints error', () async {
+            final result = await Process.run('hello', [flag]);
+            expect(
+                result.stderr,
+                equals(
+                    '❌ Parameter $bold${red}name$reset is declared as type ${gray}[double]$reset, but its default value is ${gray}[${invalid.type}]$reset\n'));
+          });
+        }
+      },
+    );
+  }
+}
