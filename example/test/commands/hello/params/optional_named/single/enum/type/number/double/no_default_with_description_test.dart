c24060b11b73dab7587720847784d70441f3cb0d
Dominik Krajcer <dom.kraj@gmail.com>
2026-01-24 21:57:46 +0100

    test(hello/params): add integration tests for optional named double enum param

---
+++ example/test/commands/hello/params/optional_named/single/enum/type/number/double/no_default_with_description_test.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../../../integration_tests.dart';
+
+void main() {
+  integrationTests(
+    '''
+        hello: ## Description of command hello
+          script: echo "Hello {name}"
+          params:
+            optional:
+              - name: '-n, --name' ## Description of parameter name
+                type: double
+                values: [2.0, -4.7]
+    ''',
+    () {
+      for (String flag in ['-n', '--name']) {
+        for (num value in [2.0, -4.7]) {
+          test('prints "Hello $value', () async {
+            final result = await Process.run('hello', [flag, '$value']);
+            expect(result.stdout, equals('Hello $value\n'));
+          });
+        }
+      }
+
+      test('prints "Hello " when no optional param is specified', () async {
+        final result = await Process.run('hello', []);
+        expect(result.stdout, equals('Hello \n'));
+      });
+
+      for (String flag in ['-n', '--name']) {
+        test('prints "Hello " when no value for optional param is specified', () async {
+          final result = await Process.run('hello', ['$flag']);
+          expect(result.stdout, equals('Hello \n'));
+        });
+
+        test('prints error when value is boolean', () async {
+          final result = await Process.run('hello', [flag, 'true']);
+          expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid value: "true"
+💡 Must be one of: $bold${green}2.0$reset, $bold${green}-4.7$reset
+'''));
+        });
+
+        for (String value in [
+          'text',
+          "text",
+          '"1"',
+          '\"1\"',
+          '\'1.5\'',
+          "'1.5'",
+          '\"true\"',
+          '"true"',
+          '\'false\'',
+          "'false'"
+        ]) {
+          test('prints error when value is string ($value)', () async {
+            final result = await Process.run('hello', [flag, value]);
+            expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid value: "$value"
+💡 Must be one of: $bold${green}2.0$reset, $bold${green}-4.7$reset
+'''));
+          });
+        }
+      }
+
+      for (String flag in ['-h', '--help']) {
+        test('$flag prints help', () async {
+          final result = await Process.run('hello', [flag]);
+          expect(result.stdout, equals('''
+${blue}hello$reset: ${gray}Description of command hello$reset
+params:
+  optional:
+    ${magenta}name (-n, --name)$reset ${gray}[double] Description of parameter name$reset
+    ${bold}values$reset: 2.0, -4.7
+'''));
+        });
+      }
+    },
+  );
+}
