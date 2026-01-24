56dc80eb00acf1cc570f1c2daf34ab61b0c5380f
Dominik Krajcer <dom.kraj@gmail.com>
2026-01-24 21:03:05 +0100

    test(hello/enum): add number enum param tests for required positional with/without default and description

---
+++ example/test/commands/hello/params/required_named/single/enum/type/number/no_default_no_description_test.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:commands_cli/colors.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../../integration_tests.dart';
+
+void main() {
+  for (String type in ['number', 'num']) {
+    integrationTests(
+      '''
+        hello:
+          script: echo "Hello {name}"
+          params:
+            required:
+              - name: '-n, --name'
+                type: $type
+                values: [1, 2.0, -3, -4.7]
+    ''',
+      () {
+        for (String flag in ['-n', '--name']) {
+          for (num value in [1, 2.0, -3, -4.7]) {
+            test('prints "Hello $value', () async {
+              final result = await Process.run('hello', [flag, '$value']);
+              expect(result.stdout, equals('Hello $value\n'));
+            });
+          }
+        }
+
+        test('shows interactive picker when no required param is specified', () async {
+          final result = await Process.run('hello', []);
+          expect(
+            result.stdout,
+            equals('''
+
+Select value for ${blue}name$reset:
+
+    ${green}1. 1    ✓$reset
+    2. 2.0   
+    3. -3    
+    4. -4.7  
+
+${gray}Press number (1-4) or press Esc to cancel:$reset
+'''),
+          );
+        });
+
+        for (String flag in ['-n', '--name']) {
+          test('prints error when no value for required param is specified', () async {
+            final result = await Process.run('hello', [flag]);
+            expect(result.stderr, equals('❌ Missing value for param: $bold${red}name$reset\n'));
+          });
+
+          test('prints error when value is boolean', () async {
+            final result = await Process.run('hello', [flag, 'true']);
+            expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid value: "true"
+💡 Must be one of: $bold${green}1$reset, $bold${green}2.0$reset, $bold${green}-3$reset, $bold${green}-4.7$reset
+'''));
+          });
+
+          for (String value in [
+            'text',
+            "text",
+            '"1"',
+            '\"1\"',
+            '\'1.5\'',
+            "'1.5'",
+            '\"true\"',
+            '"true"',
+            '\'false\'',
+            "'false'"
+          ]) {
+            test('prints error when value is string ($value)', () async {
+              final result = await Process.run('hello', [flag, value]);
+              expect(result.stderr, equals('''
+❌ Parameter $bold${red}name$reset has invalid value: "$value"
+💡 Must be one of: $bold${green}1$reset, $bold${green}2.0$reset, $bold${green}-3$reset, $bold${green}-4.7$reset
+'''));
+            });
+          }
+        }
+
+        for (String flag in ['-h', '--help']) {
+          test('$flag prints help', () async {
+            final result = await Process.run('hello', [flag]);
+            expect(result.stdout, equals('''
+${blue}hello$reset
+params:
+  required:
+    ${magenta}name (-n, --name)$reset ${gray}[number]$reset
+    ${bold}values$reset: 1, 2.0, -3, -4.7
+'''));
+          });
+        }
+      },
+    );
+  }
+}
